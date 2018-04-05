fb910ed755c3328f71deb3d098409a0e37819411
edelafuente22 <ofthefountain22@gmail.com>
2018-04-05 14:37:10 -0700

    new variables added to edit vc

--- CurlEverywhere/Controller/EditViewController.swift
+++ CurlEverywhere/Controller/EditViewController.swift
@@ -15,36 +15,43 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     var imageWidth: CGFloat = 0
     var imageHeight: CGFloat = 0
+    var imagePosition = CGPoint(x: 0, y: 0)
     var imageSize = CGSize(width: 0, height: 0)
     
     var location = CGPoint.zero
+    var newPosition = CGPoint(x: 0, y: 0)
     var stoneSize = CGSize(width: 122, height: 102)
     
     @IBOutlet weak var cancelButton: UIButton!
     
     @IBOutlet weak var selectedImage: UIImageView! {
         didSet {
-//            selectedImage.layer.borderColor = UIColor.black.cgColor
-//            selectedImage.layer.borderWidth = 0.1
-            imageWidth = selectedImage.frame.width
-            imageHeight = selectedImage.frame.height
+            imageWidth = selectedImage.bounds.width
+            imageHeight = selectedImage.bounds.height
             imageSize = CGSize(width: imageWidth, height: imageHeight)
+            imagePosition = CGPoint(x: selectedImage.bounds.origin.x, y: selectedImage.bounds.origin.y)
         }
     }
     
     @IBOutlet weak var curlingStone: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let pickerView = UIImagePickerController()
+        pickerView.delegate = self
+        selectedImage.image = imageToEdit
+    }
+    
     @IBAction func scaleCurlingStone(_ sender: UIPinchGestureRecognizer) {
         curlingStone.transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
-        let newWidth = self.view.frame.size.width
-        let newHeight = self.view.frame.size.height
+        let newWidth = self.view.bounds.size.width
+        let newHeight = self.view.bounds.size.height
         stoneSize = CGSize(width: newWidth, height: newHeight)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch : UITouch! = touches.first! as UITouch
         location = touch.location(in: self.view)
-        curlingStone.center = location
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -57,14 +64,6 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         self.saveImage(top: curlingStone.image!, bottom: imageToEdit, newSize: imageSize)
         UIImageWriteToSavedPhotosAlbum(newImage!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
         cancelButton.setTitle("Done", for: UIControlState.normal)
-    }
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let pickerView = UIImagePickerController()
-        pickerView.delegate = self
-        selectedImage.image = imageToEdit
     }
     
     @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
@@ -81,11 +80,15 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     @discardableResult
     func saveImage(top: UIImage, bottom: UIImage, newSize: CGSize) -> UIImage? {
+        let newX = curlingStone.frame.origin.x
+        let newY = curlingStone.frame.origin.y
+        newPosition = CGPoint(x: newX, y: newY)
+        
         // begin context with new size
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0.0)
         // draw images to context
-        bottom.draw(in: CGRect(origin: CGPoint.zero, size: imageSize))
-        top.draw(in: CGRect(origin: location, size: stoneSize))
+        bottom.draw(in: CGRect(origin: imagePosition, size: imageSize))
+        top.draw(in: CGRect(origin: newPosition, size: stoneSize))
         // return the new image
         newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
